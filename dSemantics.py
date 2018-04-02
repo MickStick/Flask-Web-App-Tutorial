d939e27ca1e353a4496b2921763acb4759de782b
Mikhail R. Shaw <mickstick01@hotmail.com>
2018-04-02 18:39:24 -0500

    Code fixes.

--- dSemantics.py
+++ dSemantics.py
@@ -1,101 +1,101 @@
 ## All prints are displayed in the console
 ###
 # Function that traverses brackets and perform the operations inside
-def getBrac(arr, temp, text, oldAns):
-    bracArr = []
-    newText = ""
-    newAns = ""
-    aLen = len(arr)
-    for x in range(0, aLen):
-        try:
-            if arr[x] == '(' or arr[x] == "(": #If if finds a bracket/parentheses
-                print("found")
-                #arr[x] = "_"
-                y = x + 1
-                while (y < len(arr)): #Loop for creating a new array with contents of the brackets to run the operations on
+# def getBrac(arr, temp, text, oldAns):
+#     bracArr = []
+#     newText = ""
+#     newAns = ""
+#     aLen = len(arr)
+#     for x in range(0, aLen):
+#         try:
+#             if arr[x] == '(' or arr[x] == "(": #If if finds a bracket/parentheses
+#                 print("found")
+#                 #arr[x] = "_"
+#                 y = x + 1
+#                 while (y < len(arr)): #Loop for creating a new array with contents of the brackets to run the operations on
                     
-                    print(x + 1, " - ", y) #Just checking that X and Y are in the right place
-                    print(arr)
-                    print(arr[y])
-                    if arr[y] == '(' or arr[y] == "(": #If another bracket is found then
-                        tempArr = arr #the original array is stored in a temp array
-                        tempArr[x] = '_' #and the position with the bracket replaced with "_"
-                        temp = getBrac(tempArr, temp, text, oldAns) #run the bracket function on the temp array
-                        arr = temp["array"] #gets the new array with the answers within the brackets
-                        text += temp["text"] #gets the denotinal text
-                        oldAns = temp["old"] #gets the last answer found
-                        newText += temp["text"] #this is for storing the temp denotinal text to be used in functions further down
-                        newAns = temp["old"] #this is the same as the formeer but for the last answwer found
-                        print("Inner Brackets Done!")
-                        y -= 2
-                        print("Y: ", y, "\nX: ", x)
-                        aLen = len(arr)
-                    elif arr[y] != ')' or arr[y] != ")":
-                        bracArr.append(arr[y]) #if a bracket is not found then the chracter in that position (Y fi dat) is added to the temp array "bracArr"
-                    else:
-                        break
-                    y += 1
+#                     print(x + 1, " - ", y) #Just checking that X and Y are in the right place
+#                     print(arr)
+#                     print(arr[y])
+#                     if arr[y] == '(' or arr[y] == "(": #If another bracket is found then
+#                         tempArr = arr #the original array is stored in a temp array
+#                         tempArr[x] = '_' #and the position with the bracket replaced with "_"
+#                         temp = getBrac(tempArr, temp, text, oldAns) #run the bracket function on the temp array
+#                         arr = temp["array"] #gets the new array with the answers within the brackets
+#                         text += temp["text"] #gets the denotinal text
+#                         oldAns = temp["old"] #gets the last answer found
+#                         newText += temp["text"] #this is for storing the temp denotinal text to be used in functions further down
+#                         newAns = temp["old"] #this is the same as the formeer but for the last answwer found
+#                         print("Inner Brackets Done!")
+#                         y -= 2
+#                         print("Y: ", y, "\nX: ", x)
+#                         aLen = len(arr)
+#                     elif arr[y] != ')' or arr[y] != ")":
+#                         bracArr.append(arr[y]) #if a bracket is not found then the chracter in that position (Y fi dat) is added to the temp array "bracArr"
+#                     else:
+#                         break
+#                     y += 1
 
-                print(bracArr)
-                aSize = len(bracArr) #to be used to add the contents of the temp array, after all functions have run, in the original array 
-                print("NewText (other): ", newText)
-                print("NewAns (other): ", newAns)
-                bracAns = getBrac(bracArr, temp, newText, newAns) #runs the bracket function cuz uk... brackets might be in it <most likely redundant but uk... code>
-                print("Within Brackets(Brac): ", bracArr)
-                bracAns = getProd(bracAns["array"], temp, bracAns["text"], bracAns["old"]) #runs the product function
-                print("Within Brackets(Prod): ", bracAns)
-                bracAns = getQuot(bracAns["array"], temp, bracAns["text"], bracAns["old"]) #runs the quotient function
-                print("Within Brackets(Quot): ", bracAns)
-                bracAns = getSumOrDiff(bracAns["array"], temp, bracAns["text"], bracAns["old"]) #runs the "addition or subtraction" function
-                print("Within Brackets(Sum): ", bracAns)
-                #bracArr = getDiff(bracArr)
-                #print("Within Brackets(Diff): ", bracArr)
-                text += bracAns["text"] #these are all to ensure that the correct text and last answer is sent to the funciton arguments
-                oldAns = bracAns["old"]
-                newText += bracAns["text"]
-                newAns = bracAns["old"]
-                print("NewText: ", newText)
-                print("NewAns: ", newAns)
-                print("Text: ",text)
-                print("OldAns: ",oldAns)
-                arr[y] = "_"
+#                 print(bracArr)
+#                 aSize = len(bracArr) #to be used to add the contents of the temp array, after all functions have run, in the original array 
+#                 print("NewText (other): ", newText)
+#                 print("NewAns (other): ", newAns)
+#                 bracAns = getBrac(bracArr, temp, newText, newAns) #runs the bracket function cuz uk... brackets might be in it <most likely redundant but uk... code>
+#                 print("Within Brackets(Brac): ", bracArr)
+#                 bracAns = getProd(bracArr, temp, bracAns["text"], bracAns["old"]) #runs the product function
+#                 print("Within Brackets(Prod): ", bracAns)
+#                 bracAns = getQuot(bracArr, temp, bracAns["text"], bracAns["old"]) #runs the quotient function
+#                 print("Within Brackets(Quot): ", bracAns)
+#                 bracAns = getSumOrDiff(bracArr, temp, bracAns["text"], bracAns["old"]) #runs the "addition or subtraction" function
+#                 print("Within Brackets(Sum): ", bracAns)
+#                 #bracArr = getDiff(bracArr)
+#                 #print("Within Brackets(Diff): ", bracArr)
+#                 text += bracAns["text"] #these are all to ensure that the correct text and last answer is sent to the funciton arguments
+#                 oldAns = bracAns["old"]
+#                 newText += bracAns["text"]
+#                 newAns = bracAns["old"]
+#                 print("NewText: ", newText)
+#                 print("NewAns: ", newAns)
+#                 print("Text: ",text)
+#                 print("OldAns: ",oldAns)
+#                 arr[y] = "_"
 
-                for y in range(x, len(arr)): #for loop that replaces the places where the operations were with "_" until the last index which will be replaced with the answer for the operation
-                    print("X in underscore replace: ", x)
-                    #Y equals X (So if Y minus X is not = to the size of bracArr which will also be the index to store the answer.
-                    #breaks when the answer is added to the original array.
-                    if y - x != aSize:
-                        arr[y] = "_"
-                    else:
-                        arr[y] = bracAns["array"][len(bracAns["array"]) - 1]
-                        break
+#                 for y in range(x, len(arr)): #for loop that replaces the places where the operations were with "_" until the last index which will be replaced with the answer for the operation
+#                     print("X in underscore replace: ", x)
+#                     #Y equals X (So if Y minus X is not = to the size of bracArr which will also be the index to store the answer.
+#                     #breaks when the answer is added to the original array.
+#                     if y - x != aSize:
+#                         arr[y] = "_"
+#                     else:
+#                         arr[y] = bracAns["array"][len(bracAns["array"]) - 1]
+#                         break
 
-                print("After Brackets: ", bracAns["array"])
-                print("After Brackets: ", arr)
-                bracArr = []
+#                 print("After Brackets: ", bracAns["array"])
+#                 print("After Brackets: ", arr)
+#                 bracArr = []
                 
-        except:
-            break
+#         except:
+#             break
         
 
-    newArr = [] #the array to be returned in the list
-    y = 0
-    for x in range(0, len(arr)):
-        if arr[x] == ")": #just to ensure that the ending bracket is replaced
-            arr[x] == "_"
-        if arr[x] != "_":
-            newArr.append(arr[x])
-            y += 1
+#     newArr = [] #the array to be returned in the list
+#     y = 0
+#     for x in range(0, len(arr)):
+#         if arr[x] == ")": #just to ensure that the ending bracket is replaced
+#             arr[x] == "_"
+#         if arr[x] != "_":
+#             newArr.append(arr[x])
+#             y += 1
 
-    print("NewAraay: ", newArr)
-    print("NewText: ", newText)
-    print("NewAns: ", newAns)
-    ans = { #this is the list to be returned
-        'array': newArr,
-        'text': newText,
-        'old': newAns
-    }
-    return ans
+#     print("NewAraay: ", newArr)
+#     print("NewText: ", newText)
+#     print("NewAns: ", newAns)
+#     ans = { #this is the list to be returned
+#         'array': newArr,
+#         'text': newText,
+#         'old': newAns
+#     }
+#     return ans
 
 
 ###
@@ -312,11 +312,11 @@ def getPost(text):
     temp = arr
     #arr = getBrac(arr)
     
+    # ans = getProd(arr, temp, dText, 'null')
+    # print("New Array(Prod): ", ans)
+    #ans = getBrac(arr, temp, "", 'null')
     ans = getProd(arr, temp, dText, 'null')
     print("New Array(Prod): ", ans)
-    # ans = getBrac(arr, "", 'null')
-    # ans = getProd(ans["array"], ans["text"], ans["old"])
-    # print("New Array(Prod): ", ans)
     ans = getQuot(arr, temp, ans["text"], ans["old"])
     print("New Array(Quot): ", ans)
     ans = getSumOrDiff(arr, temp, ans["text"], ans["old"])
